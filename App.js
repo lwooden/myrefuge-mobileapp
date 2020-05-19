afd950a77576c46018dd9ec3f48272c37bfd2742
lwooden <lowell.wooden@gmail.com>
2020-05-19 18:25:10 -0400

    changed to reflect NavigatorContainer structure

--- App.js
+++ App.js
@@ -1,55 +1,52 @@
+import 'react-native-gesture-handler';
 import React, {Component, useState} from 'react';
 import {View, Text, StyleSheet, Platform, StatusBar, FlatList} from 'react-native';
 import Header from './components/Header'
+import HomePage from './components/HomePage'
 import CategoryItem from './components/CategoryItem'
-import axios from 'axios'
+import PassagesScreen from './components/PassagesScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 class App extends React.Component {
   
   constructor(props) {
     super(props)
 
-  this.state = {
-    categories: []
-  }
+  // this.state = {
+  //   categories: []
+  // }
+
+
+
 }
 
-componentDidMount() {
-  this.getCategories()
-}
+// componentDidMount() {
+//   this.getCategories()
+// }
 
-getCategories() {
-  axios.get('http://localhost:3001/api/categories')
-  .then(response => {
-      this.setState({categories: response.data}, () => {
-          console.log(this.state)
-      })
-  })
-}
+// getCategories() {
+//   axios.get('http://localhost:3001/api/categories')
+//   .then(response => {
+//       this.setState({categories: response.data}, () => {
+//           console.log(this.state)
+//       })
+//   })
+// }
+
 
   render() {
+    const Stack = createStackNavigator();
 
     return (
-    <View style={styles.container}>
-      <Header />
-      <FlatList 
-        data={this.state.categories} 
-        renderItem={({item}) => <CategoryItem item={item}/>}
-      />
-     
-    </View>
+  <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={HomePage} options={{ title: 'Home' }} />
+    <Stack.Screen name="Passages" component={PassagesScreen} options={{ title: 'Passages' }} />
+    </Stack.Navigator>
+    </NavigationContainer>
   )
  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 60
-  }
-
-})
-
-
 
 export default App
