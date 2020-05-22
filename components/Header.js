79afce4a94caee4916469f66259776eb2754a10e
lwooden <lowell.wooden@gmail.com>
2020-05-22 10:38:02 -0400

    add prop to head component so we can set it dynamically

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <View style={styles.header}>
-            <Text style={styles.text}>My Refuge</Text>
+            <Text style={styles.text}>{props.title || "My Refuge"}</Text>
         </View>
     );
 };
