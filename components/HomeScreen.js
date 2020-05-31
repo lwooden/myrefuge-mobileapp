bf8cefec71c3c3ae46d9aeaf3d8e4da0bfd17c1a
lwooden <lowell.wooden@gmail.com>
2020-05-31 09:41:31 -0400

    refactored into functional component using Hooks

---
+++ components/HomeScreen.js
@@ -0,0 +1,53 @@
+import 'react-native-gesture-handler';
+import React, {useState, useEffect, useReducer} from 'react';
+import {View, Text, StyleSheet, Platform, StatusBar, FlatList} from 'react-native';
+import Header from './Header'
+import CategoryItem from './CategoryItem'
+import axios from 'axios'
+import { CardAnimationContext } from '@react-navigation/stack';
+
+
+
+const HomeScreen = () => {
+
+    const [categories, setCategories] = useState([])
+
+
+
+
+
+useEffect(() => {
+    getCategories()
+})
+
+const getCategories = () => {
+    axios.get('http://localhost:3001/api/categories?filter[order]=categoryName%20ASC')
+    .then(response => {
+        setCategories(response.data)
+    })
+}
+
+    return (
+    <View style={styles.container}>
+      <Header />
+      <FlatList 
+        data={categories} 
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({item}) => <CategoryItem item={item}/>}
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 0
+  }
+
+})
+
+
+
+
+export default HomeScreen
